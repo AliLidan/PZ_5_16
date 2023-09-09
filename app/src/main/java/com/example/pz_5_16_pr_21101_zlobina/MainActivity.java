9ca63347fa1e4c56c92a45aef7bc3dfbe675c083
Alina <alina12485615.we@gmail.com>
2023-09-09 19:18:49 +0700

    FIRST COMMIT

---
+++ app/src/main/java/com/example/pz_5_16_pr_21101_zlobina/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.pz_5_16_pr_21101_zlobina;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
